4e9c50a7d9163f69353832655dc0dbea8fa5624e
Matthew Griffiths <mrmatthewgriffiths@gmail.com>
2014-11-20 14:10:50 +0000

    * Fixing data upload

--- cellsliderdata/models.py
+++ cellsliderdata/models.py
@@ -4,8 +4,8 @@ import csv
 import tarfile
 from django.conf import settings
 from django.core.cache import cache
-from django.core.files import File
-from django.db import models
+from django.db import models, connection
+from django.utils.timezone import now
 
 
 class CSAZipFile(models.Model):
@@ -68,22 +68,29 @@ class CSADataFile(models.Model):
                 x += 1
                 if x == 0:  # headers
                     continue
-                # if x > 100000:
-                #     break
+                if x > 100000:
+                    break
                 if x % 1000 == 0:
-                    CSADataRow.objects.bulk_create(csa_data_rows)
+                    cursor = connection.cursor()
+                    statement = "INSERT INTO cellsliderdata_csadatarow (csa_id, user_name, image_name, split_number, image_url, has_cancer_count, has_fiber_count, has_blood_cell_count, amount, proportion, intensity, csa_created_at, created, updated) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+                    cursor.executemany(statement, csa_data_rows)
                     csa_data_rows = []
                     yield total_row, x
-                csa_data_row = CSADataRow(
-                    csa_id=row[0], user_name=row[1], image_name=row[2], split_number=row[3],
-                    image_url=row[4], has_cancer_count=bool(row[5] == '1'),
-                    has_fiber_count=bool(row[6] == '1'), has_blood_cell_count=bool(row[7] == '1'),
-                    amount=row[8], proportion=row[9], intensity=row[10],
-                    csa_created_at=row[11].replace(' UTC', '+00:00').replace(' ', 'T').strip())
-                if csa_data_row:
-                    csa_data_rows.append(csa_data_row)
-            if csa_data_rows:
-                CSADataRow.objects.bulk_create(csa_data_rows)
+                csa_data_rows.append((
+                    row[0],
+                    row[1],
+                    row[2],
+                    row[3],
+                    row[4],
+                    1 if row[5] == '1' else 0,
+                    1 if row[6] == '1' else 0,
+                    1 if row[7] == '1' else 0,
+                    row[8],
+                    row[9],
+                    row[10],
+                    row[11].replace(' UTC', '+00:00').replace(' ', 'T').strip(),
+                    now().isoformat(),
+                    now().isoformat()))
         yield 1, 0.1
 
 
@@ -185,7 +192,7 @@ class CSAZipFileProcess(models.Model):
         except Exception as ex:
             self._set_state(
                 CSAZipFileProcess.STATE_ERROR,
-                "There was an error importing data from the Zip File (%s) ." % ex,
+                "There was an error importing data from the Zip File.",
                 ex)
             return
 
